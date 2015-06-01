fcb7b29ebf0d8bd5ed429a6c4a40712de07519e9
Karolina <bozek.karolina@gmail.com>
2015-06-01 13:33:59 +0000

    change layout to display after log in

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -2,7 +2,7 @@ class HomepageController < ApplicationController
   
   def index
     if user_signed_in?
-      redirect_to :controller=>'dashboard', :action => 'index'
+      redirect_to :controller=>'projects', :action => 'index'
     end
   end
   
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,5 @@
 class ProjectsController < ApplicationController
-  
+   before_filter :authenticate_user!
   def index
     @projects=Project.all
   end
