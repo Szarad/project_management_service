ccf5fc439c9e7082e964bc0ce8133c3cb55733bc
Karolina <bozek.karolina@gmail.com>
2015-06-01 17:02:54 +0000

    correct redirection adding @project parameter

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -16,7 +16,7 @@ class ProjectsController < ApplicationController
     @project=Project.new(project_params)
     
     if @project.save
-      redirect_to project_path 
+      redirect_to project_path(@project)
     else
       render "new"
     end
