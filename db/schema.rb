8d9b374fd5e6b584436e5771d9008f6d07c449e1
Karolina <bozek.karolina@gmail.com>
2015-06-02 08:49:49 +0000

    create new model and controller for Ticket

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150602081433) do
+ActiveRecord::Schema.define(version: 20150602084824) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "name"
@@ -26,9 +26,9 @@ ActiveRecord::Schema.define(version: 20150602081433) do
     t.string   "priority"
     t.string   "difficulty"
     t.string   "status"
+    t.integer  "project_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.integer  "project_id"
   end
 
   create_table "users", force: :cascade do |t|
