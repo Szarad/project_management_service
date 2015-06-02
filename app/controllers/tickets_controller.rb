b91ff109b55902ffd1993e0ee501631f9cb95ec5
Karolina <bozek.karolina@gmail.com>
2015-06-02 09:01:39 +0000

    add assocciation between project and ticket and add link to display tickets

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,2 +1,17 @@
 class TicketsController < ApplicationController
+  before_filter :load_project
+    def index
+      @tickets=@project.tickets.all
+  end
+  
+   def new
+
+     @ticket = @project.tickets.new
+   end
+  
+    private
+
+  def load_project
+    @project = Project.find(params[:project_id])
+    end
 end
